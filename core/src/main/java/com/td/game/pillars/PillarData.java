1ae3bfc39cc4629839c9c357a1a1b1cfaeca0718
yusufgonen <yusuf.gonen@ug.bilkent.edu.tr>
2026-03-19 04:08:49 +0300

    updated PillarData.java for save system

---
+++ core/src/main/java/com/td/game/pillars/PillarData.java
@@ -0,0 +1,13 @@
+package com.td.game.pillars;
+
+public class PillarData {
+    public static final float BASE_ATTACK_COOLDOWN = 1.0f;
+    public static final float BASE_DAMAGE = 10f;
+    public static final float BASE_RANGE = 5f * (3.6f / 2.0f); // TILE_SIZE / 2.0
+    
+    public static final float TIER_PRIME_DAMAGE_MULT = 1.0f;
+    public static final float TIER_HYBRID_DAMAGE_MULT = 1.5f;
+
+    public static final float TIER_PRIME_RANGE_MULT = 1.0f;
+    public static final float TIER_HYBRID_RANGE_MULT = 1.25f;
+}
